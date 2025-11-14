26627f42d24f61555dc1f90396bafdee9a5f55f4
Perczll <dsafdsaddsa@gmail.com>
2025-11-14 14:46:41 +0700

    Update and rename README.md to Encryption.lua

---
+++ Encryption.lua
@@ -0,0 +1,19 @@
+local key = 123
+
+local function Encrypt(str)
+    local result = {}
+    for i = 1, #str do
+        local b = string.byte(str, i)
+        result[i] = string.char(bit32.bxor(b, key))
+    end
+    return table.concat(result)
+end
+
+local function Decrypt(str)
+    local result = {}
+    for i = 1, #str do
+        local b = string.byte(str, i)
+        result[i] = string.char(bit32.bxor(b, key))
+    end
+    return table.concat(result)
+end
